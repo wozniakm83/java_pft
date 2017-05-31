a062d820f222ca62b9f6c71c047a72686d7b3005
wozniakm83 <wozniakmichal83@gmail.com>
2017-05-31 09:26:45 +0200

    CreateNewContactTests created

---
+++ addressbook-web-tests/src/test/java/pl/pft/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package pl.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String address;
+    private final String homePhone;
+    private final String mobilePhone;
+    private final String workPhone;
+    private final String email;
+
+    public ContactData(String firstname, String lastname, String address, String homePhone, String mobilePhone, String workPhone, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.homePhone = homePhone;
+        this.mobilePhone = mobilePhone;
+        this.workPhone = workPhone;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
