79f54ef6d755363da92099d713bedb69d9153eec
wozniakm83 <wozniakmichal83@gmail.com>
2017-06-09 00:20:32 +0200

    finding max id for groups added

--- addressbook-web-tests/src/test/java/pl/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/pft/addressbook/model/GroupData.java
@@ -1,14 +1,31 @@
 package pl.pft.addressbook.model;
 
 public class GroupData {
+    private int id;
     private final String groupName;
     private final String groupHeader;
     private final String groupFooter;
 
     public GroupData(String groupName, String groupHeader, String groupFooter) {
+        this.id = 0;
         this.groupName = groupName;
         this.groupHeader = groupHeader;
         this.groupFooter = groupFooter;
+    }
+
+    public GroupData(int id, String groupName, String groupHeader, String groupFooter) {
+        this.id = id;
+        this.groupName = groupName;
+        this.groupHeader = groupHeader;
+        this.groupFooter = groupFooter;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getGroupName() {
@@ -22,4 +39,31 @@ public class GroupData {
     public String getGroupFooter() {
         return groupFooter;
     }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", groupName='" + groupName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
+        return result;
+    }
+
 }
--- addressbook-web-tests/src/test/java/pl/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -15,11 +16,16 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("Test2", "Test2_header", "Test2_footer"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Test2", "Test2_header", "Test2_footer");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
 
