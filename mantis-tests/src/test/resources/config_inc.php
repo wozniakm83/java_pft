5f7c6ff89babdec0f8a58cab43453a031120e76d
wozniakm83 <wozniakmichal83@gmail.com>
2017-07-23 17:31:03 +0200

    correction on config_inc.php file 2

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -4,6 +4,6 @@ $g_db_type = 'mysql';
 $g_db_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
-$g_db_signup_use_captcha = OFF;
+$g_signup_use_captcha = OFF;
 $g_phpmailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
